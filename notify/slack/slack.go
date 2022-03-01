e440ea46c9ab16de6cdcb50dc1685ea2cc8c41be
Hao Chen <haoel@hotmail.com>
2022-03-01 18:26:27 +0800

    support the slack notifcation

--- notify/slack/slack.go
+++ notify/slack/slack.go
@@ -1,13 +1,19 @@
 package slack
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"time"
+
 	"github.com/megaease/easeprobe/probe"
 	log "github.com/sirupsen/logrus"
 )
 
 // NotifyConfig is the slack notification configuration
 type NotifyConfig struct {
-	URL string `yaml:"url"`
+	WebhookURL string `yaml:"webhook"`
 }
 
 // Kind return the type of Notify
@@ -23,4 +29,40 @@ func (conf NotifyConfig) Config() error {
 // Notify write the message into the file
 func (conf NotifyConfig) Notify(result probe.Result) {
 	log.Infoln("Slack got the notifcation...")
+	webhookURL := "https://hooks.slack.com/services/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"
+	err := SendSlackNotification(webhookURL, result.String())
+	if err != nil {
+		log.Errorln("error %v\n", err)
+	}
+}
+
+// RequestBody is the body request Slack
+type RequestBody struct {
+	Text string `json:"text"`
+}
+
+// SendSlackNotification will post to an 'Incoming Webook' url setup in Slack Apps. It accepts
+// some text and the slack channel is saved within Slack.
+func SendSlackNotification(webhookURL string, msg string) error {
+
+	slackBody, _ := json.Marshal(RequestBody{Text: msg})
+	req, err := http.NewRequest(http.MethodPost, webhookURL, bytes.NewBuffer(slackBody))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	if buf.String() != "ok" {
+		return errors.New("Non-ok response returned from Slack")
+	}
+	return nil
 }
