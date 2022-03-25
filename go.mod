c74c024d511802a449f6fea2bc6633768362a89a
jaychung <ken8203@gmail.com>
2022-03-26 06:23:25 +0800

    feat(probe): support postgres client (#3)

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/segmentio/kafka-go v0.4.30
 	github.com/sirupsen/logrus v1.8.1
+	github.com/uptrace/bun/driver/pgdriver v1.1.2
 	go.mongodb.org/mongo-driver v1.8.4
 	gopkg.in/yaml.v2 v2.4.0
 )
@@ -18,17 +19,23 @@ require (
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/klauspost/compress v1.14.2 // indirect
 	github.com/pierrec/lz4/v4 v4.1.14 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/uptrace/bun v1.1.2 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
+	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	mellium.im/sasl v0.2.1 // indirect
 )
---
+++ probe/client/postgres/postgres.go
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2022, MegaEase
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package postgres
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/megaease/easeprobe/global"
+	"github.com/megaease/easeprobe/probe/client/conf"
+	log "github.com/sirupsen/logrus"
+	"github.com/uptrace/bun/driver/pgdriver"
+)
+
+// Kind is the type of driver
+const Kind string = "PostgreSQL"
+
+// PostgreSQL is the PostgreSQL client
+type PostgreSQL struct {
+	conf.Options  `yaml:",inline"`
+	ClientOptions []pgdriver.Option `yaml:"-"`
+}
+
+// New create a PostgreSQL client
+func New(opt conf.Options) PostgreSQL {
+	clientOptions := []pgdriver.Option{
+		pgdriver.WithNetwork("tcp"),
+		pgdriver.WithAddr(opt.Host),
+		pgdriver.WithUser(opt.Username),
+		pgdriver.WithTimeout(opt.Timeout.Round(time.Second)),
+		pgdriver.WithApplicationName(global.OrgProgVer),
+	}
+	if len(opt.Password) > 0 {
+		clientOptions = append(clientOptions, pgdriver.WithPassword(opt.Password))
+	}
+
+	tls, err := opt.TLS.Config()
+	if err != nil {
+		log.Errorf("[%s] %s - TLS Config error - %v", Kind, opt.Name, err)
+	} else {
+		clientOptions = append(clientOptions, pgdriver.WithTLSConfig(tls))
+	}
+
+	return PostgreSQL{
+		Options:       opt,
+		ClientOptions: clientOptions,
+	}
+}
+
+// Kind return the name of client
+func (r PostgreSQL) Kind() string {
+	return Kind
+}
+
+// Probe do the health check
+func (r PostgreSQL) Probe() (bool, string) {
+	db := sql.OpenDB(pgdriver.NewConnector(r.ClientOptions...))
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		return false, err.Error()
+	}
+
+	// run a SQL to test
+	if _, err := db.Query(`SELECT 1`); err != nil {
+		return false, err.Error()
+	}
+
+	return true, "Check PostgreSQL Server Successfully!"
+}
