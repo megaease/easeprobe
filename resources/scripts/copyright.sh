1058d17373a06cb6d2aa6f15abd8b46fbed52e71
Hao Chen <haoel@hotmail.com>
2022-04-10 17:10:24 +0800

    refine the script

--- resources/scripts/copyright.sh
+++ resources/scripts/copyright.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-pushd `dirname $0` > /dev/null
-SCRIPT_PATH=`pwd -P`
-popd > /dev/null
+pushd "$(dirname $0)" > /dev/null || exit
+SCRIPT_PATH=$(pwd -P)
+popd > /dev/null || exit
 
 for i in $(find ./ -name '*.go')
 do
-  if ! grep -q Copyright $i
+  if ! grep -q Copyright "$i"
   then
-    cat ${SCRIPT_PATH}/copyright.txt $i >$i.new && mv $i.new $i
+    cat "${SCRIPT_PATH}"/copyright.txt "$i" >"$i".new && mv "$i".new "$i"
   fi
 done
