8d7c5f749a0fe3d6073f3f010a538bf6ea4edb48
Hao Chen <haoel@hotmail.com>
2022-04-29 18:34:19 +0800

    bug-fixing: mysql client connection leaks (#57)
    * bug-fixing: mysql client connectin leak
    
    * go fmt fix
    
    * typo fix
    
    * fix the null pointer error if sql.Open() is error

--- probe/client/mysql/mysql.go
+++ probe/client/mysql/mysql.go
@@ -78,16 +78,20 @@ func (r MySQL) Probe() (bool, string) {
 	}
 
 	db, err := sql.Open("mysql", r.ConnStr)
+	if err != nil {
+		return false, err.Error()
+	}
 	defer db.Close()
 
 	err = db.Ping()
 	if err != nil {
 		return false, err.Error()
 	}
-	_, err = db.Query("show status like \"uptime\"") // run a SQL to test
+	row, err := db.Query("show status like \"uptime\"") // run a SQL to test
 	if err != nil {
 		return false, err.Error()
 	}
+	defer row.Close()
 
 	return true, "Check MySQL Server Successfully!"
 
