da282273dc7127acf9fa52961a9adecd8fe42d4c
Boshi Lian <farmer1992@gmail.com>
2022-05-29 20:21:12 -0700

    Revert "Update notify/teams/teams.go"
    This reverts commit 52c84e487bf811e061a6fec2ac9a9ff53aa28b84.

--- notify/teams/teams.go
+++ notify/teams/teams.go
@@ -62,7 +62,7 @@ func (c *NotifyConfig) SendTeamsMessage(title, msg string) error {
 		Type:    "MessageCard",
 		Context: "https://schema.org/extensions",
 		Title:   title,
-		Text:    report.JSONEscape(msg),
+		Text:    msg,
 	})
 
 	if err != nil {
