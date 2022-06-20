e25a098ab5dca6b9774a351b5a97645f701c2874
Hao Chen <haoel@hotmail.com>
2022-06-20 20:29:20 +0800

    add the debug info

--- global/global.go
+++ global/global.go
@@ -130,7 +130,9 @@ func ReverseMap[K comparable, V comparable](m map[K]V) map[V]K {
 // Config return a tls.Config object
 func (t *TLS) Config() (*tls.Config, error) {
 	if len(t.CA) <= 0 {
+		// the insecure is true but no ca/cert/key, then return a tls config
 		if t.Insecure == true {
+			log.Debug("[TLS] Insecure is true but the CA is empty, return a tls config")
 			return &tls.Config{InsecureSkipVerify: true}, nil
 		}
 		return nil, nil
@@ -145,6 +147,7 @@ func (t *TLS) Config() (*tls.Config, error) {
 
 	// only have CA file, go TLS
 	if len(t.Cert) <= 0 || len(t.Key) <= 0 {
+		log.Debug("[TLS] Only have CA file, go TLS")
 		return &tls.Config{
 			RootCAs:            caCertPool,
 			InsecureSkipVerify: t.Insecure,
@@ -152,6 +155,7 @@ func (t *TLS) Config() (*tls.Config, error) {
 	}
 
 	// have both CA and cert/key, go mTLS way
+	log.Debug("[TLS] Have both CA and cert/key, go mTLS way")
 	certificate, err := tls.LoadX509KeyPair(t.Cert, t.Key)
 	if err != nil {
 		return nil, err
