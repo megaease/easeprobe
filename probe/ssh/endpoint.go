5e6ec2c267624700e0557cf39b6e23ef2a619e4f
张俊杰 <57305595@qq.com>
2022-07-25 10:30:43 +0800

    Update change private key password  passphrases  ->   passphrase

--- probe/ssh/endpoint.go
+++ probe/ssh/endpoint.go
@@ -29,12 +29,12 @@ import (
 
 // Endpoint is SSH Endpoint
 type Endpoint struct {
-	PrivateKey  string      `yaml:"key"`
-	Host        string      `yaml:"host"`
-	User        string      `yaml:"username"`
-	Password    string      `yaml:"password"`
-	Passphrases string      `yaml:"passphrases"`
-	client      *ssh.Client `yaml:"-"`
+	PrivateKey string      `yaml:"key"`
+	Host       string      `yaml:"host"`
+	User       string      `yaml:"username"`
+	Password   string      `yaml:"password"`
+	Passphrase string      `yaml:"passphrase"`
+	client     *ssh.Client `yaml:"-"`
 }
 
 // ParseHost check the host is configured the port or not
@@ -82,8 +82,8 @@ func (e *Endpoint) SSHConfig(kind, name string, timeout time.Duration) (*ssh.Cli
 		// Create the Signer for this private key.
 		var signer ssh.Signer
 
-		if len(e.Passphrases) > 0 {
-			signer, err = ssh.ParsePrivateKeyWithPassphrase(key, []byte(e.Passphrases))
+		if len(e.Passphrase) > 0 {
+			signer, err = ssh.ParsePrivateKeyWithPassphrase(key, []byte(e.Passphrase))
 		} else {
 			signer, err = ssh.ParsePrivateKey(key)
 		}
