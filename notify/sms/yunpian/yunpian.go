a1234711823b609a8e6b079de155a8f8acd84e05
Hao Chen <haoel@hotmail.com>
2022-09-03 15:36:27 +0800

    adjust the debug log

--- notify/sms/yunpian/yunpian.go
+++ notify/sms/yunpian/yunpian.go
@@ -50,7 +50,7 @@ func (c Yunpian) Notify(title, text string) error {
 	form.Add("mobile", c.Mobile)
 	form.Add("text", c.Sign+text)
 
-	log.Debugf("[%s] - API %s - Form %s", c.Kind(), api, form)
+	log.Debugf("[%s / %s] - API %s - Form %s", c.Kind(), c.Name(), api, form)
 	req, err := http.NewRequest(http.MethodPost, api, strings.NewReader(form.Encode()))
 	if err != nil {
 		return err
